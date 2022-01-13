1a564c3cb9c61411cbbd23f4cb3f2ac64e9abe23
Dana <sundetbayeva.dana@yandex.ru>
2022-01-13 17:32:24 +0500

    fixbugs with db

--- utils/configs.js
+++ utils/configs.js
@@ -1,6 +1,5 @@
 const VERIFY_CONST = 'd4fa66d51031952d32fcdec3ed808f94394fd8777b3bdfe7ad6a6eb798a93481';
-// const MONGODB_URL = 'mongodb://localhost:27017/moviesdb';
-const MONGODB_URL = 'mongodb://api.dana.movieservice.nomoredomains.rocks/moviesdb';
+const MONGODB_URL = 'mongodb://localhost:27017/moviesdb';
 
 module.exports = {
   VERIFY_CONST,
