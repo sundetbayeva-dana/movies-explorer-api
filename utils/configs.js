2307a210ac969e1deaae43be63b4d7ee8eaa1d27
Dana <sundetbayeva.dana@yandex.ru>
2022-01-16 22:36:53 +0500

    ready to check

--- utils/configs.js
+++ utils/configs.js
@@ -1,9 +1,7 @@
 const VERIFY_CONST = 'd4fa66d51031952d32fcdec3ed808f94394fd8777b3bdfe7ad6a6eb798a93481';
 const MONGODB_URL = 'mongodb://localhost:27017/moviesdb';
-const PORT = 3000;
 
 module.exports = {
   VERIFY_CONST,
   MONGODB_URL,
-  PORT,
 };
