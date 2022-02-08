f3c66d00751c42b994166071e7a7f43ab4636f3d
Dana <sundetbayeva.dana@yandex.ru>
2022-02-08 21:15:51 +0500

    убрано sameSite true

--- controllers/users.js
+++ controllers/users.js
@@ -85,7 +85,7 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : VERIFY_CONST, { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true });
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
       res.send({
         data: {
           email: user.email, name: user.name,
