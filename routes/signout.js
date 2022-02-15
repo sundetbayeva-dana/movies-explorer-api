9df2b45ee87b5f0b9e739247c92f58d2ef3c8d54
Dana <sundetbayeva.dana@yandex.ru>
2022-02-15 22:34:42 +0500

    исправлен роут signout

--- routes/signout.js
+++ routes/signout.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/api/signout', (req, res) => {
+router.post('/api/signout', (req, res) => {
   res.status(200).clearCookie('jwt').send({ message: 'Выход' });
 });
 
